6a1e1420339b1fb616e4007605d5386bda2e3dd7
Gebhard Wöstemeyer <g.woestemeyer@gmail.com>
2013-06-24 14:46:01 +0200

    add sync_charities job

--- boostify.gemspec
+++ boostify.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'rails', '~> 3.2.12'
   s.add_dependency 'haml'
+  s.add_dependency 'faraday'
 
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'rspec-rails'
@@ -30,4 +31,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'fabrication'
   s.add_development_dependency 'faker'
   s.add_development_dependency 'database_cleaner'
+  s.add_development_dependency 'webmock'
 end
---
+++ lib/boostify/jobs/sync_charities.rb
@@ -0,0 +1,38 @@
+module Boostify
+  module Jobs
+    class SyncCharities
+
+      def self.perform
+        Boostify.favorite_charities.each do |charity_id|
+          update_or_create_charity charity_id
+        end
+      end
+
+      private
+
+        def self.update_or_create_charity(charity_id)
+          charity = Charity.where(boost_id: charity_id).first_or_initialize
+          charity.update_attributes! params(charity_id)
+        end
+
+        def self.params(charity_id)
+          params = JSON.parse(download(charity_id))['charity']
+          params['boost_id'] = params['id']
+          params
+        end
+
+        def self.download(charity_id)
+          response = Faraday.get charity_url(charity_id)
+          unless response.success?
+            raise "Failed to download Charity with ID=#{charity_id}!\n" <<
+              "#{response.inspect}"
+          end
+          response.body
+        end
+
+        def self.charity_url(charity_id)
+          "#{Boostify.charity_api_endpoint}/#{charity_id}.json"
+        end
+    end
+  end
+end
