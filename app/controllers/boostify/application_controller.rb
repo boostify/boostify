09af7b4728c0556c61b5d1a00a63795a24f0f6f6
schasse <sebastian.schasse@gapfish.com>
2013-06-25 17:24:35 +0200

    reviewable

--- app/controllers/boostify/application_controller.rb
+++ app/controllers/boostify/application_controller.rb
@@ -1,4 +1,4 @@
 module Boostify
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
   end
 end
--- app/controllers/boostify/donations_controller.rb
+++ app/controllers/boostify/donations_controller.rb
@@ -1,4 +1,40 @@
 module Boostify
   class DonationsController < ApplicationController
+
+    # in params complete donation needed (with charity, amount, etc.)
+    # in session donatable_id needed
+    def create
+      #TODO validate some presents...
+      if @donation = Donation.create(donation_params)
+        create_transaction @donation
+        session[:donatable_id] = nil
+        redirect_to donation_path(@donation), notice: pixel_url
+      else
+        redirect_to charities_path
+      end
+    end
+
+    def show
+      @donation = Donation.find params[:id]
+      #TODO redirect to boost, if no charity given
+    end
+
+    private
+
+      def donation_params
+        params.require(:donation).permit(:amount, :commission, :charity_id)
+          .merge(donatable_id: session[:donatable_id])
+      end
+
+      def create_transaction(donation)
+        #TODO create a transaction to the donation
+        # do not forget to do some validations
+        # (p.e. user is allowed to create a transaction)
+      end
+
+      def pixel_url
+        src = @donation.pixel_url
+        render_to_string partial: 'pixel_img', locals: { src: src.html_safe}
+      end
   end
 end
