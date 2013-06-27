6d4eec888c9dc88a18833033995777a031b31198
schasse <sebastian.schasse@gapfish.com>
2013-06-27 10:19:35 +0200

    session workaround

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Boostify::Engine.routes.draw do
 
   resources :charities, only: [:index, :show]
 
-  resources :donations, only: [:create, :show]
+  resources :donations, only: [:new, :create, :show]
 end
