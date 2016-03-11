094f316419c1eba77da96ecf2742e4b80aae0b4d
Frank C. Eckert <frank.eckert@boost-project.com>
2016-03-11 12:29:18 +0100

    forgot one occurence of secret

--- app/helpers/boostify/signature_verification_helper.rb
+++ app/helpers/boostify/signature_verification_helper.rb
@@ -1,8 +1,16 @@
 module Boostify
   module SignatureVerificationHelper
 
+    def partner_secret
+      if Boostify.partner_secret.respond_to? :call
+        Boostify.partner_secret.call
+      else
+        Boostify.partner_secret
+      end
+    end
+
     def verify_signature!
-      HMACAuth::Signature.verify(params, secret: Boostify.partner_secret) ||
+      HMACAuth::Signature.verify(params, secret: partner_secret) ||
         head(:unprocessable_entity)
     end
 
